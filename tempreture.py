dc202aa60984818682fe86ba9d19094ad0563af6
Daniel Kofi Boadu <mrboadu3@gmail.com>
2022-08-21 01:55:26 -0400

    reformed code from using functions to classes

--- tempreture.py
+++ tempreture.py
@@ -1,68 +1,32 @@
 import requests
 
-def location_weather():
-    
-    try:
-        location= input("Enter location: ")
-        api_request= requests.get(f"https://weatherdbi.herokuapp.com/data/weather/{location}")
-        
-        weather_data= api_request.json()
-        
-        return weather_data
-    
-    except requests.exceptions.ConnectionError:
-        
-        print("Could not connect to the server at this time")
-    
-    
-def weather_methods():
-
-    reports= location_weather()
-
-    weather_keys=[]
-
-    for key, value in reports.items():
-        
-        weather_keys.append(key)
-
-    week_reports= reports[weather_keys[2]]
+class TempretureReports:
+    def __init__(self,location):
+        self.location= location
+        self._apiRequests= requests.get(f"https://weatherdbi.herokuapp.com/data/weather/{self.location}")
+        self._apiReport= self._apiRequests.json()
 
 
-    return week_reports
-
-        
-    
-def organizeReports():
-
-    data= weather_methods()
-
-    for items in data:
-
-        for key,value in items.items():
-
-            days= items["day"]
-            max_temp=items["max_temp"]["f"]
-            min_temp= items["min_temp"]["f"]
-
-        results= print(f"\n{days} \n The highest temp= {max_temp}\n The lowest temp={min_temp}")
+    def weatherData(self):
+        reports= self._apiReport
+        weatherData=[]
+        [weatherData.append(key) for key, value in reports.items()]
+        weekData=reports[weatherData[2]]
+        return weekData
 
 
-    return results
-    
-    
+    def weekTempReport(self):
+        data= self.weatherData()
+        for items in data:
+            for _,_ in items.items():
+                days= items["day"]
+                max_temp=items["max_temp"]["f"]
+                min_temp= items["min_temp"]["f"]
+            finalReport= print(f"\n{days} \n The highest temp= {max_temp}\n The lowest temp={min_temp}")
+        return finalReport
 
 
-def tempretureReports():
-    
-    organizeReports()
-        
-  
-        
-      
-    
-    
-tempretureReports()
-    
-  
-    
+
+
+
     
