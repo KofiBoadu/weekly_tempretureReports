f87802bad7639d1b4cc93f9e09c243c62ad427f7
Daniel Kofi Boadu <mrboadu3@gmail.com>
2022-08-21 08:54:54 -0400

    added comments

--- tempreture.py
+++ tempreture.py
@@ -1,11 +1,15 @@
 import requests
 
+#Locally installed the request package using pip install request and imported it to make our API request
+
 class TempretureReports:
     def __init__(self,location):
         self.location= location
         self._apiRequests= requests.get(f"https://weatherdbi.herokuapp.com/data/weather/{self.location}")
         self._apiReport= self._apiRequests.json()
 
+#our class has a property that returns a JSON file with weather data that can be further manipulated with 
+# further methods if needful
 
     def weatherData(self):
         reports= self._apiReport
@@ -13,6 +17,7 @@ class TempretureReports:
         [weatherData.append(key) for key, value in reports.items()]
         weekData=reports[weatherData[2]]
         return weekData
+    # the weatherData method returns only the tempreture data from the API JSON file 
 
 
     def weekTempReport(self):
@@ -25,6 +30,9 @@ class TempretureReports:
             finalReport= print(f"\n{days} \n The highest temp= {max_temp}\n The lowest temp={min_temp}")
         return finalReport
 
+        
+#The weekTempReport method manipulates the JSON WEATHER DATA file and returns the formated verison of 
+#only the highest and lowest tempreture in FAHAREIT 
 
 
 
